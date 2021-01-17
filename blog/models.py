35d8b5a82efa4c7e52447db81298af3e93fc2e8e
gassdev <gassdev@gmail.com>
2021-01-17 23:59:14 +0000

    Post model and migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class PostQuerySet(models.QuerySet):
+
+    def delete(self, *args, **kwargs):
+        for obj in self:
+            obj.img.delete()
+        super(PostQuerySet, self).delete(*args, **kwargs)
+
+class Post(models.Model):
+    objects = PostQuerySet.as_manager()
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    img = models.ImageField(default='default.jpg', upload_to='post/images/')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+    def delete(self, *args, **kwargs):
+        if self.img.name.split('/')[-1] != 'default.jpg':
+            self.img.delete()
+        super().delete(*args, **kwargs)  # Call the "real" save() method.
--- blog/views.py
+++ blog/views.py
@@ -1,32 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'Gassdev',
-        'title': 'Article du Blog 1',
-        'content': 'Premier article du blog',
-        'date_posted': '07 Janvier 2021',
-        'image': 'http://placehold.it/300x300'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Article du Blog 2',
-        'content': 'Deuxième article du blog',
-        'date_posted': '08 Janvier 2021',
-        'image': 'http://placehold.it/300x300'
-    },
-    {
-        'author': 'John Doe',
-        'title': 'Article du Blog 3',
-        'content': 'Troisième article du blog',
-        'date_posted': '09 Janvier 2021',
-        'image': 'http://placehold.it/300x300'
-    }
-]
+
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
