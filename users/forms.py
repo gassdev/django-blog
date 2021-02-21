e8dc4fe6072ad731c55e4306d4aa4e91145688db
gassdev <gassdev@gmail.com>
2021-02-21 22:43:04 +0000

    update user profile and resize the image on save

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import  Profile
 
 User._meta.get_field('email')._unique = True
 
@@ -8,30 +9,22 @@ User._meta.get_field('email')._unique = True
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
-    # def __init__(self, *args, **kwargs):
-    #     super(UserCreationForm, self).__init__(*args, **kwargs)
-
-    #     self.fields['username'].label = "Pseudo"
-    #     self.fields['username'].er
-    #     self.fields['username'].help_text = "Obligatoire. 150 caractères ou moins. Lettres, chiffres et @ /. / + / - / _ uniquement."
-
-
-    #     self.fields['email'].label = "Adresse e-mail"
-
-
-    #     self.fields['password1'].label = "Mot de passe"
-    #     self.fields['password1'].help_text = """
-    #         <ul>
-    #             <li>Votre mot de passe ne peut pas être trop similaire à vos autres informations personnelles.</li>
-    #             <li>Votre mot de passe doit contenir au moins 8 caractères.</li>
-    #             <li>Votre mot de passe ne peut pas être un mot de passe couramment utilisé.</li>
-    #             <li>Votre mot de passe ne peut pas être entièrement numérique.</li>
-    #         </ul>
-    #     """
-
-    #     self.fields['password2'].label = "Confirmez votre mot de passe"
-    #     self.fields['password2'].help_text = "Entrez le même mot de passe que précédemment, pour vérification."
-
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -8,3 +9,24 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    
+    def delete(self, *args, **kwargs):
+        if self.image.name.split('/')[-1] != 'default.jpg':
+            self.image.delete()
+        super().delete(*args, **kwargs)
+    
+    def save(self, *args, **kwargs):
+        try:
+            this = Profile.objects.get(id=self.id)
+            if (this.image != self.image) and (this.image.name.split('/')[-1] != 'default.jpg'):
+                this.image.delete(save=False)
+        except: pass
+        super(Profile, self).save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+        
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
